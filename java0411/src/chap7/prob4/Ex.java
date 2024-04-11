dd6d7bbce78e5745f0c05cfa2d82683b85993200
choidaehan1 <155132549+choidaehan1@users.noreply.github.com>
2024-04-11 12:10:15 +0900

    Add files via upload

---
+++ java0411/src/chap7/prob4/Ex.java
@@ -0,0 +1,12 @@
+package chap7.prob4;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		AService bs = new AService();
+		controller.setService(bs);
+
+	}
+
+}
---
+++ java0411/src/sec02/exam04/Vehicle.java
@@ -0,0 +1,7 @@
+package sec02.exam04;
+
+public class Vehicle { // 차량
+	public void run() {
+		System.out.println("차량이 달린다.");
+	}
+}
