6db4b3b0c29980e36f30262dcca76b5077b2a0c7
choidaehan1 <155132549+choidaehan1@users.noreply.github.com>
2024-04-24 10:23:52 +0900

    Add files via upload

---
+++ java0424/src/sec01/exam07/ExitExample.java
@@ -0,0 +1,16 @@
+package sec01.exam07;
+
+
+public class ExitExample {
+	public static void main(String[] args)  {
+		for(int i=0; i<10; i++) {
+			System.out.println(i);
+			if(i == 5) {
+				System.exit(0);
+//				break;
+			}
+		}
+		System.out.println("프로그램 종료");
+	}
+}
+
